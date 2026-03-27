c27c8f94d542800791fd316e5127854d58218706
K4LC <b2220240@photon.chitose.ac.jp>
2026-03-27 20:40:28 +0900

    モデル名、作成者名の追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,8 +5,9 @@ function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
-      <TextInput />
+      <h1>GDT CVAT</h1>
+      <TextInput label='モデル名' placeholder='model.onnx'/>
+      <TextInput label='作成者名' placeholder='Ishikubo'/>
     </>
   )
 }
--- frontend/src/TextInput.tsx
+++ frontend/src/TextInput.tsx
@@ -1,5 +1,17 @@
-function TextInput(){
-    return <button type="button">クリック</button>
+type Props = {
+    label: string
+    placeholder?: string
+}
+
+function TextInput({ label, placeholder = ""}: Props) {
+    return (
+        <div>
+            <label>
+                {label}:
+                <input type="text" placeholder={placeholder} />
+            </label>
+        </div>
+    )
 }
 
 export default TextInput;
