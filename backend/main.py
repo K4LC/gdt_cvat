4b95e518bcb3beed5e11c7a01e2a2b9f50c927df
K4LC <b2220240@photon.chitose.ac.jp>
2026-03-29 23:47:59 +0900

    バックエンドに送信

--- backend/main.py
+++ backend/main.py
@@ -2,11 +2,9 @@ from fastapi import FastAPI, UploadFile, File, Form
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
-# 👇 これをルーティングより前に書く
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://192.168.0.11:8060"],  # ← これ重要
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -16,7 +14,13 @@ app.add_middleware(
 async def generate(
     modelName: str = Form(...),
     author: str = Form(...),
-    svgFiles: UploadFile = File(None),
-    onnxFiles: UploadFile = File(None),
+    svg: UploadFile = Form(...),
+    onnx: UploadFile = Form(...)
 ):
-    return {"status": "ok"}
+    print("modelName:", modelName)
+    print("author:", author)
+
+    print("svg FileName:", svg.filename)
+    print("onnx FileName:", onnx.filename)
+
+    return {"message": "ok"}
--- frontend/src/DeployGeneratePage.tsx
+++ frontend/src/DeployGeneratePage.tsx
@@ -1,24 +1,79 @@
+import { useState} from "react";
+import axios from "axios";
+
 function DeployGeneratePage() {
-  
+  const [modelName, setModelName] = useState<string>("");
+  const [author, setAuthor] = useState<string>("");
+  const [svgFile, setSvgFile] = useState<File | null>(null);
+  const [onnxFile, setOnnxFile] = useState<File | null>(null);
+
+  const validateFile = (file: File | null, ext: string) => {
+    if (!file) {
+      alert("ファイルを選択してください");
+      return false;
+    }
+
+    const fileName = file.name.toLowerCase();
+    if (!fileName.endsWith(`.${ext}`)) {
+      alert(`${ext}ファイルを選択してください`);
+      return false;
+    }
+
+    return true;
+  };
+
+  const handleGenerate = async () => {
+    if (modelName == "" || author == "" || !svgFile || !onnxFile) {
+      alert("すべて埋めてください");
+      return;
+    }
+    if (!validateFile(svgFile, "svg")) return;
+    if (!validateFile(onnxFile, "onnx")) return;
+
+    const formData = new FormData();
+    formData.append("modelName", modelName);
+    formData.append("author", author);
+    formData.append("svg", svgFile);
+    formData.append("onnx", onnxFile);
+
+    try {
+      const res = await axios.post(
+        `http://localhost:${import.meta.env.VITE_API_PORT}/generate`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      console.log("backendに送信成功", res.data);
+    } catch (err) {
+      console.log("backendに送信失敗", err)
+    }
+  };
+
   return (
     <div>
       <div>
       <label>モデル名</label>
-      <input type="text" placeholder="model" />
+      <input type="text" placeholder="model" onChange={(e) => setModelName(e.target.value)}/>
       </div>
       <div>
       <label>作成者名</label>
-      <input type="text" placeholder="Ishikubo" />
+      <input type="text" placeholder="Ishikubo" onChange={(e) => setAuthor(e.target.value)} />
       </div>
       
       <div>
       <label>svgファイル</label>
-      <input type="file" />
+      <input type="file" accept=".svg" onChange={(e) => setSvgFile(e.target.files?.[0] || null)}/>
       </div>
       <div>
       <label>モデルファイル</label>
-      <input type="file" />
+      <input type="file" accept=".onnx" onChange={(e) => setOnnxFile(e.target.files?.[0] || null)}/>
       </div>
+
+      <button onClick={handleGenerate}>Generate</button>
     </div>
   );
 }
